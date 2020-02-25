0d68339885197978fce15cd9e5997b8042514d08
ViktorHryhorovych <48521164+ViktorHryhorovych@users.noreply.github.com>
2020-02-25 18:12:16 +0200

    first test

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static public int Example(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
+            return c;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть перше число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введiть друге число: ");
+            int b = int.Parse(Console.ReadLine());
+            Example(a, b);
+            Console.ReadLine();
+        }
+    }
+}
